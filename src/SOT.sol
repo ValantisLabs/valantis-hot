21566b73415f0f7e8bf34c180663440156c9e169
happenwah <tiagolixa@hotmail.com>
2023-12-19 13:57:20 +0000

    remove: remove unnecessary foundry libs

--- src/SOT.sol
+++ src/SOT.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
+import { IERC20 } from 'valantis-core/lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
+
 import { ISovereignPool } from 'valantis-core/src/pools/interfaces/ISovereignPool.sol';
 
 contract SOT {}
