86420e5ff6c8890a76f52b4254cbe31be52ecb93
Tanishk Goyal <goyaltanishk02@gmail.com>
2024-02-14 20:14:37 +0530

    chore: added upper bound on signature timestamp, to bound it into a fixed range

--- src/libraries/SOTParams.sol
+++ src/libraries/SOTParams.sol
@@ -25,6 +25,7 @@ library SOTParams {
     error SOTParams__validateBasicParams_quoteExpired();
     error SOTParams__validateBasicParams_unauthorizedSender();
     error SOTParams__validateBasicParams_unauthorizedRecipient();
+    error SOTParams__validateBasicParams_invalidSignatureTimestamp();
     error SOTParams__validateFeeParams_insufficientFee();
     error SOTParams__validateFeeParams_invalidfeeGrowthInPips();
     error SOTParams__validateFeeParams_invalidFeeMax();
@@ -56,6 +57,10 @@ library SOTParams {
 
         if (sot.expiry > maxDelay) revert SOTParams__validateBasicParams_excessiveExpiryTime();
 
+        if (sot.signatureTimestamp > block.timestamp) revert SOTParams__validateBasicParams_invalidSignatureTimestamp();
+
+        // Also equivalent to: signatureTimestamp >= block.timestamp - maxDelay
+        // So, block.timestamp - maxDelay <= signatureTimestamp <= block.timestamp
         if (block.timestamp > sot.signatureTimestamp + sot.expiry) revert SOTParams__validateBasicParams_quoteExpired();
 
         if (amountOut > tokenOutMaxBound) revert SOTParams__validateBasicParams_excessiveTokenOutAmountRequested();
