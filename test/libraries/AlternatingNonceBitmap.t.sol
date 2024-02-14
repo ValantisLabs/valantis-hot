82a691b8a06cb69b4ad7b6fb38ff8ea7386879ad
Tanishk Goyal <goyaltanishk02@gmail.com>
2024-02-14 20:15:50 +0530

    refactor: refactored library tests, so that they can be analyzed by forge coverage

--- test/libraries/AlternatingNonceBitmap.t.sol
+++ test/libraries/AlternatingNonceBitmap.t.sol
@@ -6,42 +6,60 @@ import { console } from 'forge-std/console.sol';
 
 import { AlternatingNonceBitmap } from 'src/libraries/AlternatingNonceBitmap.sol';
 
+contract AlternatingNonceBitmapHarness {
+    function flipNonce(uint56 bitmap, uint8 nonce) external pure returns (uint56) {
+        uint56 result = AlternatingNonceBitmap.flipNonce(bitmap, nonce);
+        return result;
+    }
+
+    function checkNonce(uint56 bitmap, uint8 nonce, uint8 expectedFlag) external pure returns (bool) {
+        bool result = AlternatingNonceBitmap.checkNonce(bitmap, nonce, expectedFlag);
+        return result;
+    }
+}
+
 contract TestAlternatingNonceBitmap is Test {
+    AlternatingNonceBitmapHarness harness;
+
+    function setUp() public {
+        harness = new AlternatingNonceBitmapHarness();
+    }
+
     function test__checkNonce_general() public {
         uint56 bitmap = 8; // 1000
 
-        assertTrue(AlternatingNonceBitmap.checkNonce(bitmap, 3, 1), 'incorrect nonce check 1');
-        assertFalse(AlternatingNonceBitmap.checkNonce(bitmap, 2, 1), 'incorrect nonce check 2');
+        assertTrue(harness.checkNonce(bitmap, 3, 1), 'incorrect nonce check 1');
+        assertFalse(harness.checkNonce(bitmap, 2, 1), 'incorrect nonce check 2');
     }
 
     function test__checkNonce_edgeCaseMax() public {
         uint56 bitmap = 0xffffffffffffff; // 1 at 55th bit
 
-        assertTrue(AlternatingNonceBitmap.checkNonce(bitmap, 55, 1), 'incorrect nonce check 3');
-        assertFalse(AlternatingNonceBitmap.checkNonce(bitmap, 55, 0), 'incorrect nonce check 4');
+        assertTrue(harness.checkNonce(bitmap, 55, 1), 'incorrect nonce check 3');
+        assertFalse(harness.checkNonce(bitmap, 55, 0), 'incorrect nonce check 4');
     }
 
     function test__checkNonce_edgeCaseMin() public {
         uint56 bitmap = 1;
 
-        assertTrue(AlternatingNonceBitmap.checkNonce(bitmap, 0, 1), 'incorrect nonce check 5');
-        assertFalse(AlternatingNonceBitmap.checkNonce(bitmap, 0, 0), 'incorrect nonce check 6');
+        assertTrue(harness.checkNonce(bitmap, 0, 1), 'incorrect nonce check 5');
+        assertFalse(harness.checkNonce(bitmap, 0, 0), 'incorrect nonce check 6');
     }
 
     function test__checkNonce_generalMixed() public {
         uint56 bitmap = 13; // 1101
 
-        assertTrue(AlternatingNonceBitmap.checkNonce(bitmap, 0, 1), 'incorrect nonce check 7');
-        assertTrue(AlternatingNonceBitmap.checkNonce(bitmap, 1, 0), 'incorrect nonce check 8');
-        assertTrue(AlternatingNonceBitmap.checkNonce(bitmap, 2, 1), 'incorrect nonce check 9');
-        assertTrue(AlternatingNonceBitmap.checkNonce(bitmap, 3, 1), 'incorrect nonce check 10');
+        assertTrue(harness.checkNonce(bitmap, 0, 1), 'incorrect nonce check 7');
+        assertTrue(harness.checkNonce(bitmap, 1, 0), 'incorrect nonce check 8');
+        assertTrue(harness.checkNonce(bitmap, 2, 1), 'incorrect nonce check 9');
+        assertTrue(harness.checkNonce(bitmap, 3, 1), 'incorrect nonce check 10');
     }
 
     function test__checkNonce_exceedingNonceValue() public {
         uint56 bitmap = 8; // 1000
 
         vm.expectRevert(AlternatingNonceBitmap.AlternatingNonceBitmap__checkNonce_nonceOutOfBounds.selector);
-        AlternatingNonceBitmap.checkNonce(bitmap, 56, 1);
+        harness.checkNonce(bitmap, 56, 1);
     }
 
     function test__checkNonce_incorrectExpectedValue() public {
@@ -49,32 +67,32 @@ contract TestAlternatingNonceBitmap is Test {
 
         vm.expectRevert(AlternatingNonceBitmap.AlternatingNonceBitmap__checkNonce_expectedFlagInvalid.selector);
 
-        AlternatingNonceBitmap.checkNonce(bitmap, 4, 2);
+        harness.checkNonce(bitmap, 4, 2);
     }
 
     function test__flipNonce_general() public {
         uint56 bitmap = 8; // 1000
 
-        assertEq(AlternatingNonceBitmap.flipNonce(bitmap, 3), 0, 'incorrect nonce flip 1');
-        assertEq(AlternatingNonceBitmap.flipNonce(bitmap, 2), 12, 'incorrect nonce flip 2');
+        assertEq(harness.flipNonce(bitmap, 3), 0, 'incorrect nonce flip 1');
+        assertEq(harness.flipNonce(bitmap, 2), 12, 'incorrect nonce flip 2');
     }
 
     function test__flipNonce_exceedingNonceValue() public {
         uint56 bitmap = 8; // 1000
 
         vm.expectRevert(AlternatingNonceBitmap.AlternatingNonceBitmap__flipNonce_nonceOutOfBounds.selector);
-        AlternatingNonceBitmap.flipNonce(bitmap, 56);
+        harness.flipNonce(bitmap, 56);
     }
 
     function test__flipNonce_fuzz(uint56 bitmap, uint8 nonce) public {
+        uint56 expectedBitmap;
         if (nonce >= 56) {
             vm.expectRevert(AlternatingNonceBitmap.AlternatingNonceBitmap__flipNonce_nonceOutOfBounds.selector);
+            expectedBitmap = harness.flipNonce(bitmap, nonce);
+        } else {
+            // If the same nonce is flipped twice, we should get back the same bitmap.
+            expectedBitmap = harness.flipNonce(bitmap, nonce);
+            assertEq(bitmap, harness.flipNonce(expectedBitmap, nonce), 'incorrect nonce flip 3');
         }
-        // If the same nonce is flipped twice, we should get back the same bitmap.
-        assertEq(
-            bitmap,
-            AlternatingNonceBitmap.flipNonce(AlternatingNonceBitmap.flipNonce(bitmap, nonce), nonce),
-            'incorrect nonce flip 3'
-        );
     }
 }
