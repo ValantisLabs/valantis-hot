1dfea570df4109d3666ee9a7780d198950b11841
Tanishk Goyal <goyaltanishk02@gmail.com>
2024-02-24 23:40:46 +0530

    chore: simplified tightPack logic, using bitmasks

--- src/libraries/utils/TightPack.sol
+++ src/libraries/utils/TightPack.sol
@@ -7,6 +7,9 @@ import { AMMState } from 'src/structs/SOTStructs.sol';
     @notice Helper library for tight packing multiple uint160 values into minimum amount of uint256 slots.
  */
 library TightPack {
+    uint256 constant LOWER_160_MASK = 0x000000000000000000000000ffffffffffffffffffffffffffffffffffffffff;
+    uint256 constant UPPER_96_MASK = 0xffffffffffffffffffffffff0000000000000000000000000000000000000000;
+
     /************************************************
      *  FUNCTIONS
      ***********************************************/
@@ -16,7 +19,7 @@ library TightPack {
         @param a uint160 value to pack into slot1.
         @param b uint160 value to pack into slot1 and slot2.
         @param c uint160 value to pack into slot2.
-        @dev slot1: << 32 flag bits | upper 64 bits of b | all 160 bits of a >>
+        @dev slot1: << 32 free bits | upper 64 bits of b | all 160 bits of a >>
              slot2: << lower 96 bits of b | all 160 bits of c >>
      */
     function setState(AMMState storage state, uint160 a, uint160 b, uint160 c) internal {
@@ -45,8 +48,8 @@ library TightPack {
         uint256 slot2 = state.slot2;
 
         assembly {
-            a := shr(96, shl(96, slot1)) // TODO: Add mask here
-            c := shr(96, shl(96, slot2)) // TODO: Add mask here
+            a := and(slot1, LOWER_160_MASK)
+            c := and(slot2, LOWER_160_MASK)
             b := or(shl(96, shr(160, slot1)), shr(160, slot2))
         }
     }
@@ -54,14 +57,14 @@ library TightPack {
     function getSqrtSpotPriceX96(AMMState storage state) internal view returns (uint160 a) {
         uint256 slot1 = state.slot1;
         assembly {
-            a := shr(96, shl(96, slot1)) // TODO: Add mask here
+            a := and(slot1, LOWER_160_MASK)
         }
     }
 
     function setSqrtSpotPriceX96(AMMState storage state, uint160 a) internal {
         uint256 slot1 = state.slot1;
         assembly {
-            slot1 := or(shl(160, shr(160, slot1)), a)
+            slot1 := or(and(slot1, UPPER_96_MASK), a)
         }
         state.slot1 = slot1;
     }
